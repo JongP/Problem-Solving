fe56a7d239941a6962c9c788c5ef9d5054c52d89
JongP <jong.p@kaist.ac.kr>
2021-11-25 19:26:09 +0900

    53 Maximum Subarray

---
+++ dp/53.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l=len(nums)
+        if l==1:
+            return nums[0]
+        dp=[0]*l#0th:
+        dp[0]=nums[0]
+        maxNum=dp[0]
+        for i in range(1,l):
+            dp[i]=max(nums[i],nums[i]+dp[i-1])
+            if maxNum<dp[i]:
+                maxNum=dp[i]
+            
+        return maxNum
